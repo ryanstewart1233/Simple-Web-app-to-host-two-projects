f801cadd83c086f3fc4aaa5c302deccd61f38395
Ryan Stew <stewart1233@hotmail.co.uk>
2020-05-16 10:40:36 +0100

    first Commit

---
+++ templates/extra.py
@@ -0,0 +1,8 @@
+    <title>Flask App</title>
+  <link rel="stylesheet" href="{{url_for('static',filename='css/main.css')}}">
+</head>
+<body>
+<header>
+
+
+<li><a href="{{ url_for('home') }}">Home</a></li>
