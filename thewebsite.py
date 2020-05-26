1a16a21e0bd4a4550a2567e5072a1d672cbbc7eb
Ryan Stew <stewart1233@hotmail.co.uk>
2020-05-26 22:28:19 +0100

    Added Hollys etsy app

--- thewebsite.py
+++ thewebsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, send_file
 
 
 
@@ -114,15 +114,78 @@ def plot():
 def home():
     return render_template("home.html")
 
-@app.route('/about/')
+@app.route('/content/')
 def first_page():
     return render_template("content.html")
 
-@app.route('/plot/', methods=['Stock_symbol'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+#@app.route('/plot/', methods=['Stock_symbol'])
+#def my_form_post():
+    #text = request.form['text']
+    #processed_text = text.upper()
+    #return processed_text
+@app.route('/etsydata/', methods =["POST", "GET"])
+def etsy_data():
+    import requests
+    from bs4 import BeautifulSoup
+    import pandas as pd
+    from datetime import datetime
+
+    if request.method == "POST":
+        shop_name = request.form.get("shop_name")
+
+        shop_name=str(shop_name)
+
+        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"}
+        URL = "https://www.etsy.com/uk/shop/"+shop_name
+
+        r=requests.get("https://www.etsy.com/uk/shop/"+shop_name, headers=headers)
+        c=r.content
+        soup = BeautifulSoup(c, 'html.parser')
+        base_url = "https://www.etsy.com/uk/shop/"+shop_name+"?page="
+
+        l=[]
+        pg_nr=soup.find_all("p", {"class": "wt-action-group__item"})[0].text
+        page_nr=str(pg_nr)[-2:]
+        for page in range(1,(int(page_nr)+1)):
+            #print(base_url+str(page)+"#items")
+            r = requests.get(base_url+str(page)+"#items", headers=headers)
+            c = r.content
+            soup = BeautifulSoup(c, 'html.parser')
+            all = soup.find_all("a", {"class":"display-inline-block listing-link"})
+            #basket=soup.find_all("div", {"class":"text-danger text-body-smaller"})
+            for item in all:
+                d={}
+                try:
+                    d["Product_Name"]=item.find_all("h3")[0].text.replace("\n", "").replace(" ", "")
+                except:
+                    d["Product_Name"]=None
+                try:
+                    d["Product_Price"]=item.find_all("span", {"class": "currency-value"})[0].text
+                except:
+                    d["Product_Price"]=None
+                try:
+                    d["Item_Status"]=item.find_all("span", {"class": "display-inline-block"})[0].text
+                except:
+                    d["Item_Status"]=None
+                l.append(d)
+        df=pd.DataFrame(l)
+        df
+        date=(str(datetime.now())[:10])
+        global filename
+        filename=(shop_name + date +".csv")
+        df.to_csv(filename)
+        return render_template("etsy_data.html", text="Please download your spreadsheet", btn="download.html" )
+    else:
+        return render_template("etsy_data.html")
+
+@app.route("/download-file/")
+def download():
+    print(filename)
+    print(type(filename))
+    return send_file(filename_or_fp=filename, attachment_filename=filename, mimetype="csv", as_attachment=True)
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
+    #when deployed change debug to False
